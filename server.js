984c25cc38be8f34ac990e129accd090239c54ee
s150909 <s150909@berufskolleg-borken.de>
2019-03-09 14:40:07 +0100

    ...

--- server.js
+++ server.js
@@ -1,31 +1,37 @@
-// einzeiliger Kommentar
-/*Mehrzeiliger 
-Kommentar*/
-//Infos un anderen Programieren zeigen wie etwas funktionert
+const express = require('express')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 
-//Anweisungen nach oben nach untnten abgearbeitet
-// zweitesn tool
-
-const express = require('express')
-//helfen frame work , gint es auch als Internetseite
-//müssen dafür aber erst auf der Internet seite
 const app = express()
-//bindegleid zwischen den clied und nodejs
-app.use(bodyParser.urlencoded({extendet: true}))
-app.use(express.static('public'))
 app.set('view engine', 'ejs')
-//Daten aus der Datenbank an die html formular
-app.use(express.static('puplic'))
-//ab jetzt sucht die app di statishen inhalte unterm puplic ordner
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extendet: true}))
+app.use(cookieParser())
+
+
+
 const server = app.listen(process.env.PORT || 3000, () => {
-    console.log('Der Server ist erfolgreich gestartet auf Port %s', server.address().port)
+    console.log('Server lauscht auf Port %s', server.address().port)
 })
+
 app.get('/',(req, res, next) => {
-    res.render('index.ejs', {
+
+    let idKunde = req.cookies['istAngemeldetAls']
+
+    if(idKunde){
+        console.log("Kunde ist angemeldet als " + idKunde)
+        res.render('index.ejs', {
+        })
+    }else{
+        res.render('login.ejs', {
 
     })
+    }
+    
+
+   
+
+   
 })
 
 app.get('/login',(req, res, next) => {
@@ -41,7 +47,7 @@ app.post('/',(req, res, next) => {
 
     if(idKunde === "4711" && kennwort ==="123"){
         console.log("Der Cookie wird gesetzt")
-        res.cookie('istAngemeldetAls', 'idKunde')
+        res.cookie('istAngemeldetAls',idKunde)
         res.render('index.ejs', {
         })
     }else{
